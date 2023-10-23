4fe8e9677ba47660921cb2b36aefd4e4bb7b1c71
Luan-art <luanlf2003@gmail.com>
2023-10-23 10:40:10 -0300

    Desativação necessidade certificado TSL

--- app/src/main/java/br/edu/ifsp/tcc/apprepublic/Api/RESTService.java
+++ app/src/main/java/br/edu/ifsp/tcc/apprepublic/Api/RESTService.java
@@ -1,20 +1,25 @@
     package br.edu.ifsp.tcc.apprepublic.Api;
     
-    import java.io.IOException;
-    
-    import retrofit2.Call;
-    import retrofit2.Response;
+
     import retrofit2.Retrofit;
     import retrofit2.converter.gson.GsonConverterFactory;
+    import okhttp3.OkHttpClient;
+    import okhttp3.CertificatePinner;
     
     public class RESTService {
         private static final String BASE_URL = "https://192.168.0.19:8080/";
-    
+
+        static OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .hostnameVerifier((hostname, session) -> true)  // Desabilitar a verificação do nome do host
+                .certificatePinner(new CertificatePinner.Builder().build())  // Ignorar a verificação do certificado
+                .build();
+
         private static Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
                 .build();
-    
+
         public static HomeService getHomeService() {
             return retrofit.create(HomeService.class);
         }
